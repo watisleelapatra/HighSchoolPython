a514584e077bbc68c41712b0c129ce501f2c584e
watisleelapatra <69517963+watisleelapatra@users.noreply.github.com>
2022-08-27 15:20:35 +0700

    Update Ex09-distance.py

--- Ex09-distance.py
+++ Ex09-distance.py
@@ -4,7 +4,6 @@ x1 = float(input("Enter x1: "))
 y1 = float(input("Enter y1: "))
 x2 = float(input("Enter x2: "))
 y2 = float(input("Enter y2: "))
-distance = ((x2-x1)**2+(y2-y1**2))**(1/2)
+distance = ((x2-x1)**2+(y2-y1)**2)**(1/2)
 print("distance = ", end = '')
 print(distance)
-
