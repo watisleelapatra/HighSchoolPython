8c66c3c69c4cde12cc65a3a84b633e6ff79403bb
watisleelapatra <69517963+watisleelapatra@users.noreply.github.com>
2022-08-28 10:31:52 +0700

    Update Ex13-BMI.py

--- Ex13-BMI.py
+++ Ex13-BMI.py
@@ -10,7 +10,7 @@ elif bmi >= 18.5 and bmi < 24.9 :
     print("you are normal")
 elif bmi >= 25 and bmi < 29.9 :
     print("you are overweight")
-elif bmi > 30 and bmi < 34.9 :
+elif bmi >= 30 and bmi < 34.9 :
     print("you are obese")
 elif bmi > 35 :
     print("you are extremely obese")
